6fadd361577136ad0d9631b6abb0d2def567ecf2
OksanaBakhaieva <obahaeva@gmail.com>
2024-06-22 22:00:34 +0100

    update model

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -1,33 +1,37 @@
 import { Schema, model } from 'mongoose';
 
 const contactSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
+        name: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+        },
+        isFavourite: {
+            type: Boolean,
+            default: false,
+        },
+        contactType: {
+            type: String,
+            enum: ['work', 'home', 'personal'],
+            required: true,
+            default: 'personal',
+        },
     },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-    },
-    isFavourite: {
-        type: Boolean,
-        default: false,
-    },
-    contactType: {
-        type: String,
-        enum: ['work', 'home', 'personal'],
-        required: true,
-        default: 'personal',
-    },
-    createdAt: {
-        timestamps: true,
-    },
-    updatedAt: {
-        timestamps: true,
-    },
+    {
+    timestamps: true,
+    versionKey: false,
+    // createdAt: {
+    //     timestamps: true,
+    // },
+    // updatedAt: {
+    //     timestamps: true,
+    // },
 });
 
 const Contact = model("contact", contactSchema);
--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import env from './utils/env.js';
-import contacts from './db/contacts.js';
+
+import { getContacts } from './services/contact-services.js';
 
 const port = env('PORT', '3000');
 
@@ -18,12 +19,13 @@ const setupServer = () => {
     app.use(logger);
     app.use(cors());
 
-    app.get('/', (res, req) => {
-        req.send([]);
-    });
+    // app.get('/', (req, res) => {
+    //     req.send([]);
+    // });
 
-    app.get('/api/contacts', (res,req) => {
-        req.json(contacts);
+    app.get('/api/contacts', async (req, res) => {
+        const result = await getContacts();
+        res.json(result);
     });
 
     app.use((req, res) => {
