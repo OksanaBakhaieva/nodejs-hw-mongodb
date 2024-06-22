fe9e61c46d57e742b50633cb63b9f5172873c7a9
OksanaBakhaieva <obahaeva@gmail.com>
2024-06-22 23:52:56 +0100

    update getContacts

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import pino from 'pino-http';
 import env from './utils/env.js';
 
-import { getContacts } from './services/contact-services.js';
+import { getContactByID, getContacts } from './services/contact-services.js';
 
 const port = env('PORT', '3000');
 
@@ -24,8 +24,40 @@ const setupServer = () => {
     // });
 
     app.get('/api/contacts', async (req, res) => {
-        const result = await getContacts();
-        res.json(result);
+        const data = await getContacts();
+        res.json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data,
+        });
+    });
+
+    app.get('/api/contacts/:contactId', async (req, res) => {
+        try {
+            const { contactId } = req.params;
+            const data = await getContactByID(contactId);
+
+            if (!data) {
+                return res.status(404).json({
+                    status: 404,
+                    message: `Contact with id ${contactId} not found`
+                });
+            }
+            res.json({
+                status: 200,
+                message: `Successfully found contact with id ${contactId}!`,
+                data,
+            });
+        }
+        catch (error) {
+            if (error.message.includes("Cast to ObjectId failed")) {
+                error.status = 404;
+            }
+            const { status = 500 } = error;
+            res.status(status).json({
+                message: error.message
+            });
+        };
     });
 
     app.use((req, res) => {
