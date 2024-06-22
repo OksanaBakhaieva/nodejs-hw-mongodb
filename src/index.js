1180d998cc8a80370878df19bb78cfc2a3b5d07e
OksanaBakhaieva <obahaeva@gmail.com>
2024-06-22 23:56:25 +0100

    clear main branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import initMongoCollection from "./db/initMongoCollection.js";
-import setupServer from "./server.js";
-
-const bootstrap = async () => {
-    await initMongoCollection();
-    setupServer();
-};
-
-bootstrap();
--- src/services/contact-services.js
+++
@@ -1,3 +0,0 @@
-import Contact from '../db/models/Contact.js';
-
-export const getContacts = () => Contact.find();
