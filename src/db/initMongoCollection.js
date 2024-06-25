da212b12f8b3e82269d6fccd94056c2ffe8f2daf
OksanaBakhaieva <obahaeva@gmail.com>
2024-06-25 18:54:58 +0100

    remove time out for mongoose

--- src/db/initMongoCollection.js
+++ src/db/initMongoCollection.js
@@ -8,13 +8,7 @@ const initMongoCollection = async () => {
         const url = env("MONGODB_URL");
         const db = env("MONGODB_DB");
         const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
-        await mongoose.connect(DB_HOST,
-            {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            connectTimeoutMS: 30000 // 30 секунд
-        }
-        );
+        await mongoose.connect(DB_HOST);
         console.log("Mongo connection successfully established!");
     }
     catch (error) {
